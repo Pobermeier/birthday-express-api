c3707465f042d834d8ebedaecbd471c4ca9fa8f7
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-03 15:33:01 +0200

    feat: Add GET & POST routes

--- server.js
+++ server.js
@@ -17,14 +17,35 @@ app.use(bodyParser.json());
 // @desc Get all data - names + birthday
 app.get('/birthday', (req, res) => {
   console.log('GET /birthday');
-  res.send('GET /birthday');
+  if (data) {
+    res.status(200).json(data);
+  } else {
+    res.status(500).json({ msg: 'Internal Server Error (500)' });
+  }
 });
 
 // @route POST /birthday
 // @desc Add a new entry & Save name + birthday
 app.post('/birthday', (req, res) => {
   console.log('POST /birthday');
-  res.send('POST /birthday');
+  const newUserData = req.body;
+  console.log(newUserData);
+
+  if (!req.body.first_name || !req.body.last_name || !req.body.birthday) {
+    res.status(400).json({ msg: 'Invalid data!' });
+    return;
+  }
+
+  newUserData.id = generateUUID();
+  data &&
+    data.push({
+      id: generateUUID(),
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      birthday: req.body.birthday,
+    });
+
+  res.status(200).json(data);
 });
 
 // @route DELETE /birthday/:id
